fcb2b79a03de1203d0b8428c348fbde182160f7c
qwer946 <khalid.ali.987@gmail.com>
2022-05-10 09:53:41 -0500

    making notes on function I did

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let container = document.getElementById("container");
 
+// we made an API call.
 const settings = {
   async: true,
   crossDomain: true,
@@ -13,11 +14,13 @@ const settings = {
 //that settings is getting information on the API called (body).
 $.ajax(settings).done(function (body) {
   console.log(body.response);
-  // what we need is to get the API called response, whats under response is what we need to make our fixtures.
+  // what we need is to get the API called response (JSON), whats under response is what we need to make our fixtures.
   let fullData = body.response;
   //  to make it easier we say full data instead of body.response
 
   //  what we need to do is create a function that could list the team name score and logo.
+
+  //   we loop through every single fixture, forEach fixture data (game), we are looking homeTeam information by accessing it by objects using teams.home.name.
   fullData.forEach(function (fixtureData) {
     let homeTeam = {
       name: fixtureData.teams.home.name,
@@ -44,18 +47,19 @@ $.ajax(settings).done(function (body) {
     // populate that element with above parsed data, ie.. "awayTeam.name", "HomeTeam.logo"
 
     // append that new element with the populated data to the document (hint: use .append(you element goes here))
-
+    // this console is the ending it with the ajax.
     console.log(homeTeam);
     console.log(awayTeam);
     console.log("current time is " + currentMin + " \n");
   });
 });
 
-//  do we do let kickOff = settings
+//  we translate this to our webpage. we add a varaible and combine all hometeam,awayteam,and the timer.
 function addMatches(homeTeam, awayTeam, currentMin) {
+  //   we create our div template and create a copy of the template. I also removed Id because we cant have the 2 names that are same (templates)
   let fixtureHtml = document.getElementById("template").cloneNode(true);
   fixtureHtml.removeAttribute("id");
-
+  // We created a varible called homeTeamLogo. We said go into the template called .homeBadge and put in a logo by setting up the attribe and putting the object. the TextContent is what we use to put it the website.
   let homeTeamLogo = fixtureHtml.querySelector(".homeBadge");
   homeTeamLogo.setAttribute("src", homeTeam.logo);
   let homeTeamName = fixtureHtml.querySelector(".homeClub");
